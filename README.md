89ebc518b2b5f74b2c784156da9f6e61087d743a
seungjo <f1v3@kakao.com>
2024-11-18 20:14:46 +0900

    payment service

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # toby-spring
+
+
+- 외부 API를 통해 환율 정보를 가져옴 (https://open.er-api.com/v6/latest/USD)
+- record
--- src/main/java/seungjo/hello/PaymentService.java
+++ src/main/java/seungjo/hello/PaymentService.java
@@ -1,21 +1,44 @@
 package seungjo.hello;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.math.BigDecimal;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.time.LocalDateTime;
+import java.util.stream.Collectors;
 
 public class PaymentService {
 
-    public Payment prepare(Long oderId, String currency, BigDecimal foreignCurrencyAmount) {
+    public Payment prepare(Long oderId, String currency, BigDecimal foreignCurrencyAmount) throws IOException {
 
         // todo 1. 환율 가져오기
-        // todo 2. 금액 계산
-        // todo 3. 유효 시간 계산
+        URL url = new URL("https://open.er-api.com/v6/latest/" + currency);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 
-        return new Payment(oderId, currency, foreignCurrencyAmount,
-                BigDecimal.ZERO, BigDecimal.ZERO, LocalDateTime.now());
+        BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String response = br.lines().collect(Collectors.joining());
+        br.close();
+
+        // 우리가 필요한 정보(관심사)만 빼오자.
+        ObjectMapper mapper = new ObjectMapper();
+        ExRateData data = mapper.readValue(response, ExRateData.class);
+        BigDecimal exRate = data.rates().get("KRW");
+
+
+        // todo 2. 금액 계산
+        BigDecimal convertedAmount = foreignCurrencyAmount.multiply(exRate);
+
+        // todo 3. 유효 시간 계산
+        LocalDateTime validUntil = LocalDateTime.now().plusMinutes(30);
+
+        return new Payment(oderId, currency, foreignCurrencyAmount, exRate, convertedAmount, validUntil);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         PaymentService paymentService = new PaymentService();
 
         Long orderId = 100L;
