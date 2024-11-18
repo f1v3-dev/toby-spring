e1109584247b79fcb5f678ee50b204be2b88629f
seungjo <f1v3@kakao.com>
2024-11-18 16:04:15 +0900

    init commit

--- src/main/java/seungjo/hello/HelloApplication.java
+++ src/main/java/seungjo/hello/HelloApplication.java
@@ -1,13 +1,13 @@
-package seungjo.spring;
+package seungjo.hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class HelloApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(HelloApplication.class, args);
     }
 
 }
--- src/test/java/seungjo/hello/HelloApplicationTest.java
+++ src/test/java/seungjo/hello/HelloApplicationTest.java
@@ -1,10 +1,10 @@
-package seungjo.spring;
+package seungjo.hello;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApplicationTests {
+class HelloApplicationTest {
 
     @Test
     void contextLoads() {
